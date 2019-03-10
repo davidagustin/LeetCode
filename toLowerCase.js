7be585a399208860fb8b4a0685b744b80237bab0
David Agustin <davidsyagustin@gmail.com>
2019-03-10 04:29:58 -0700

    Finish toLowerCase

---
+++ toLowerCase.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+  return str.toLowerCase()
+};
+
+/*
+const toLowerCase = (str) => {
+    //Calculate Ascii diff from a to A
+    const DIFF = ('a'.charCodeAt(0) - 'A'.charCodeAt(0));
+
+    return Array
+        .from(str)
+        .map( ch => ( ch >= 'A' && ch <= 'Z' ) ? String.fromCharCode(ch.charCodeAt(0) + DIFF) : ch )
+        .join('');
+};
+*/
