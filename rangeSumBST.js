0bcb2cca9ed14493841632a21356bab291ed6a0c
David Agustin <davidsyagustin@gmail.com>
2019-03-10 04:06:20 -0700

    Added Optimal Solution for rangeSumBST

--- rangeSumBST.js
+++ rangeSumBST.js
@@ -32,3 +32,22 @@ var rangeSumBST = function (root, L, R) {
     return acc + value
   })
 };
+
+/*
+var rangeSumBST = function (root, L, R) {
+  if (root === null) {
+    return 0;
+  }
+
+
+  if (root.val < L) {
+    return rangeSumBST(root.right, L, R);
+  }
+
+  if (root.val > R) {
+    return rangeSumBST(root.left, L, R);
+  }
+
+  return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+};
+*/
