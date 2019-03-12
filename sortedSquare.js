23feeb8ea113982c7b3682a50bd7af2907aa57d3
David Agustin <davidsyagustin@gmail.com>
2019-03-11 18:44:43 -0700

    Implement sortedSquares Optimized

--- sortedSquare.js
+++ sortedSquare.js
@@ -12,3 +12,27 @@ var sortedSquares = function(A) {
     return a - b
   });
 };
+
+/*
+var sortedSquares = function(A) {
+    let res = [];
+    let up = 0, down = A.length-1;
+
+    let a = A[up]*A[up];
+    let b = A[down]*A[down];
+
+    while (up <= down) {
+        if (a > b) {
+            res.push(a);
+            up++;
+            a = A[up]*A[up];
+        } else {
+            res.push(b);
+            down--;
+            b = A[down]*A[down];
+        }
+    }
+
+    return res.reverse();
+};
+*/
