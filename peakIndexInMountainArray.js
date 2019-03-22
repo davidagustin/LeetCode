4eced4b742047a3e91a47c2f5b482821108de444
David Agustin <davidsyagustin@gmail.com>
2019-03-21 19:11:28 -0700

    Find optimal solution for peakIndexInMountainArray

--- peakIndexInMountainArray.js
+++ peakIndexInMountainArray.js
@@ -15,3 +15,26 @@ var peakIndexInMountainArray = function(A) {
   }
   return highestIndex;
 };
+
+/*
+var peakIndexInMountainArray = function(A) {
+  function mid(a,b) {
+    return Math.floor((b-a)/2);
+  }
+
+  let left = 0, right = A.length-1;
+  let pivot = mid(left, right);
+
+  while ( A[pivot-1]>A[pivot] || A[pivot]<A[pivot+1] ) {
+    if(A[pivot]>A[pivot+1]) {
+      right = pivot;
+      pivot = mid(left, right)
+    } else {
+      left = pivot;
+      pivot += mid(left, right)
+    }
+  }
+
+  return pivot;
+};
+*/
